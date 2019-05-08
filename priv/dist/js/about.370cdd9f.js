c6adc95979f33262db31facddd42e07260208de5
Velasco Carlos <carlos.velasco@afconsult.com>
2019-05-08 11:22:41 +0200

    cowboy webclient updated

---
+++ priv/dist/js/about.370cdd9f.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("div",{staticClass:"about"},[e("h1",[t._v("\n    This is an about page\n  ")])])}],u=e("2877"),c={},i=Object(u["a"])(c,a,s,!1,null,null,null);n["default"]=i.exports}}]);
+//# sourceMappingURL=about.370cdd9f.js.map
